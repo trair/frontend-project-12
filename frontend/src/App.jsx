9bf99c381fa47266eb2981ff128bb3a7c3336e29
Maxim Nikonov <the_roffler@mail.ru>
2023-02-16 23:35:43 +0300

    changed Login and add image

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,29 @@
-import { BrowserRouter, Route, Routes, } from 'react-router-dom';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import React from 'react';
+import {
+  BrowserRouter, Route, Routes,
+} from 'react-router-dom';
+import { Navbar } from 'react-bootstrap';
 import Login from './pages/Login';
-import NotFoundPage from './pages/NotFoundPage';
+import NotFound from './pages/NotFound';
 
-const App = () => {
-  return (
-    <>
+const App = () => (
+  <div className="h-100">
+    <div className="d-flex flex-column h-100">
+      <Navbar
+        className="shadow-sm navbar navbar-expand-lg navbar-light bg-white"
+      >
+        My chat
+      </Navbar>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
-          <Route path="*" element={<NotFoundPage />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
-  );
-};
+    </div>
+  </div>
+);
 
 export default App;
