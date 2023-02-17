af71a15bb38be83ea01c2a616098a9283d68e6ba
Maxim Nikonov <the_roffler@mail.ru>
2023-02-17 18:01:02 +0300

    add channels and messages

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -1,6 +1,25 @@
+import useAuth from '../hooks/index.jsx';
+import axios from 'axios';
+
+const getChannelsAndMessages = async (token) => {
+  const response = await axios.get('/api/v1/data', {
+    headers: {
+      'Authorization': `Bearer ${token}`,
+    }
+  })
+
+  console.log(response.data);
+}
+
 const Chat = () => {
+  const auth = useAuth();
+  const { token } = auth.userData;
+  getChannelsAndMessages(token);
+
   return (
-    <div>Ghat</div>
+    <div>
+      Chat
+    </div>
   )
 };
 
