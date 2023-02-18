63ae8c5a899ec1c8d935935919472e5247ee4d1c
Maxim Nikonov <the_roffler@mail.ru>
2023-02-18 20:50:45 +0300

    implement dropdown in channel

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -3,18 +3,20 @@ import { useSelector } from 'react-redux';
 const Messages = () => {
   const currentChannelID = useSelector((state) => state.channels.currentChannelId);
   const messages = useSelector((state) => state.messages);
-
+  
   return (
     messages
       .filter(({ channelId }) => channelId === currentChannelID)
       .map((message, index) => (
-      <div className='text-break mb-2' key={index}>
-        <div className='message-container'></div>
+      <div className='text-break mb-2 ' key={index}>
+        <div>
           <b>{`${message.username}: `}</b>
+        </div>
+        <div className='message'>
           {message.body}
+        </div>
       </div>
     ))
-  )
-};
+)};
 
 export default Messages;
