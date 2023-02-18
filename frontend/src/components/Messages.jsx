fc6ab17b11a3c6cdfff175fd04ee944a35111436
Maxim Nikonov <the_roffler@mail.ru>
2023-02-18 20:32:56 +0300

    edit some files

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -1,10 +1,13 @@
 import { useSelector } from 'react-redux';
 
 const Messages = () => {
+  const currentChannelID = useSelector((state) => state.channels.currentChannelId);
   const messages = useSelector((state) => state.messages);
 
   return (
-    messages.map((message, index) => (
+    messages
+      .filter(({ channelId }) => channelId === currentChannelID)
+      .map((message, index) => (
       <div className='text-break mb-2' key={index}>
         <b>{`${message.username}: `}</b>
         {message.body}
