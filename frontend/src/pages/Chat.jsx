93036b8df566520445908d546cd39427c20140b3
Maxim Nikonov <the_roffler@mail.ru>
2023-02-19 19:10:51 +0300

    Logout

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -2,15 +2,19 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import useAuthContext from '../hooks/index.jsx';
 import fetchAuthorizationData from '../redux/thunk.js';
+
 import InputMessages from '../components/InputMessage';
 import Channels from "../components/Channels.jsx";
 import ChatInfo from "../components/ChatInfo.jsx";
 import Messages from "../components/Messages.jsx";
 import AddChannel from "../components/AddChannel";
+
+import Nav from '../components/Nav';
+
 import { addMessage } from "../redux/slices/messagesSlice.js";
 import { addChannel, deleteChannel, renameChannel } from "../redux/slices/channelsSlice.js";
-import { io } from "socket.io-client";
 
+import { io } from "socket.io-client";
 const socket = io();
 
 const Chat = () => {
@@ -39,15 +43,17 @@ const Chat = () => {
   }, [data, dispatch]);
 
   return (
-    <div className="container h-100 my-4 overflow-hidden rounded shadow">
+    <>
+      <Nav button={true}/>
+      <div className="container h-100 my-4 overflow-hidden rounded shadow">
       <div className="row h-100 bg-white flex-md-row">
         <div className="col-4 col-md-2 border-end pt-5 px-0 bg-light">
           <div className="d-flex justify-content-between mb-2 ps-4 pe-2">
             <span>Каналы</span>
-            <AddChannel socket={socket}/>
+            <AddChannel socket={socket} />
           </div>
           <ul className="nav flex-column nav-pills nav-fill px-2">
-            <Channels socket={socket}/>
+            <Channels socket={socket} />
           </ul>
         </div>
         <div className="col p-0 h-100">
@@ -57,12 +63,14 @@ const Chat = () => {
               <Messages />
             </div>
             <div className="mt-auto px-5 py-3">
-              <InputMessages socket={socket}/>
+              <InputMessages socket={socket} />
             </div>
           </div>
         </div>
       </div>
     </div>
+    </>
+
   )
 };
 
