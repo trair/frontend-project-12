e80393ab16ce42b43516fe0c8dcea5d8d6a2bd76
Maxim Nikonov <the_roffler@mail.ru>
2023-02-19 19:47:22 +0300

    return back messagesSlice

--- frontend/src/redux/slices/messagesSlice.js
+++ frontend/src/redux/slices/messagesSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 import fetchAuthorizationData from '../thunk.js';
 
 import { deleteChannel } from './channelsSlice.js';
@@ -9,19 +9,17 @@ const messagesSlice = createSlice({
   reducers: {
     addMessage: (state, { payload }) => {
       state.push(payload);
-    },
+    }
   },
   extraReducers: (builder) => {
-    builder
-      .addCase(fetchAuthorizationData.fulfilled, (state, { payload }) => payload.messages);
-    builder
-      .addCase(deleteChannel, (state, { payload }) => {
-        const filtered = state.filter(({ channelId }) => channelId !== payload.id);
-        return filtered;
-      });
+    builder.addCase(fetchAuthorizationData.fulfilled, (state, { payload }) => {
+      return payload.messages;
+    }),
+    builder.addCase(deleteChannel, (state, { payload }) => {
+      return state.filter(({ channelId }) => channelId !== payload.id);
+    })
   },
 });
 
-export const messagesSelector = (state) => state.messages;
-export const { addMessage } = messagesSlice.actions;
+export const { addMessage }  = messagesSlice.actions;
 export default messagesSlice.reducer;
