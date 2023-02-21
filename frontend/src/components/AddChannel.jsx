a3f712b9cdf04602e581a25d1d98027f5c1024e7
Maxim Nikonov <the_roffler@mail.ru>
2023-02-21 18:31:52 +0300

    add react-bootstrap in login page

--- frontend/src/components/AddChannel.jsx
+++ frontend/src/components/AddChannel.jsx
@@ -3,7 +3,6 @@ import { useFormik } from 'formik';
 import { useSelector } from 'react-redux';
 
 import Button from 'react-bootstrap/Button';
-//import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import Alert from 'react-bootstrap/Alert';
@@ -44,11 +43,11 @@ const AddChannel = ({ socket }) => {
         <Modal.Header closeButton>
           <Modal.Title>Добавить канал</Modal.Title>
         </Modal.Header>
-          <Form onSubmit={handleSubmit}>
+        <Form onSubmit={handleSubmit}>
           <Modal.Body>
             <Form.Group className="mb-3" >
               <Form.Control
-                className={ errors.channelName ? 'form-control is-invalid' : '' }
+                className={ errors.channelName && 'form-control is-invalid' }
                 id="channelName"
                 type="text"
                 value={values.channelName}
@@ -56,19 +55,19 @@ const AddChannel = ({ socket }) => {
                 autoComplete="off"
                 autoFocus
                 onChange={handleChange}
-              />
+                />
             </Form.Group>
             <Alert show={ !!errors.channelName } variant='danger'>{errors.channelName}</Alert>
-            </Modal.Body>
-            <Modal.Footer>
+          </Modal.Body>
+          <Modal.Footer>
             <Button type="button" variant="secondary" onClick={toggleModal}>
-                Отменить
-              </Button>
-              <Button type="submit" variant="success" disabled={!isValid}>
-                Добавить
-              </Button>
-            </Modal.Footer>
-          </Form>
+              Отменить
+            </Button>
+            <Button type="submit" variant="success" disabled={!isValid}>
+              Добавить
+            </Button>
+          </Modal.Footer>
+        </Form>
       </Modal>
     </>
   );
