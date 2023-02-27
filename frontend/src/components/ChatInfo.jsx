e8e6b1f7963877a244bcfb7ce0a0b828807a8cb5
Maxim Nikonov <the_roffler@mail.ru>
2023-02-27 10:59:52 +0300

    fix ChatInfo

--- frontend/src/components/ChatInfo.jsx
+++ frontend/src/components/ChatInfo.jsx
@@ -5,14 +5,14 @@ const ChatInfo = () => {
   const { t } = useTranslation();
   const { channels, currentChannelId } = useSelector((state) => state.channels);
   const messages = useSelector((state) => state.messages);
-  const channel = channels.find((channel) => channel.id === currentChannelId);
+  const channel = channels.find(({ id }) => id === currentChannelId);
 
   const countMessages = messages.filter(({ channelId }) => channelId === currentChannelId).length;
 
   return (
     <div className="bg-light mb-4 p-3 shadow-sm small">
       <p className="m-0">
-      <b>{channel ? `# ${channel.name}` : ''}</b>
+        <b>{channel ? `# ${channel.name}` : ''}</b>
       </p>
       <span className="text-muted">{`${countMessages} ${t('messages')}`}</span>
     </div>
